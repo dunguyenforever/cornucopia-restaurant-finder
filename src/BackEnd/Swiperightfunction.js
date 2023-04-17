062f2d2a4ec4cd740b7856948b3c6c3073e26db0
Huy Le <89702079+Huy-Le2@users.noreply.github.com>
2023-04-17 01:36:07 -0400

    bug fixed swiping function and added input reading

--- src/BackEnd/Swiperightfunction.js
+++ src/BackEnd/Swiperightfunction.js
@@ -27,5 +27,43 @@ function swipeRight() {
       swipeRight();
       xStart = null; // Reset the starting X coordinate
     }
+      document.addEventListener('touchstart', function(event) {
+  // Define a custom confirm message
+  const message = "Are you sure you want to proceed?";
+
+  // Call the built-in confirm function with the custom message
+  const result = confirm(message);
+
+  // If the user chooses to cancel, prevent the touch event from triggering
+  if (!result) {
+    event.preventDefault();
   }
+          document.addEventListener("touchstart", function(event) {
+  // Record the starting X coordinate of the touch
+  const startX = event.touches[0].clientX;
+  
+  // Add a listener for the 'touchend' event
+  document.addEventListener("touchend", function(event) {
+    // Record the ending X coordinate of the touch
+    const endX = event.changedTouches[0].clientX;
+    
+    // Check if the user swiped right
+    if (endX > startX) {
+      handleSwipeRight(); // Call the swipe right handler function
+    }
+  }, false);
+}, false);
+      // Define a function to track user input
+function trackInput() {
+  let userInput = ""; // Initialize user input as an empty string
+
+  // Add a listener for the 'input' event on the input field
+  const inputField = document.getElementById("myInput");
+  inputField.addEventListener("input", function(event) {
+    userInput = event.target.value; // Update user input with the new value
+    console.log(userInput); // Log the updated user input to the console
+  });
+}
+  }
+
   
