3449057a8f9b09c57b98e34396e7e6084ba1b3f0
Nhat HQ Nguyen <91711623+huuquangnhatnguyen@users.noreply.github.com>
2023-04-20 23:30:24 -0400

    Cornucopia website
    Embed the Sign-in Modal into the main pages

---
+++ src/FrontEnd/WebAppSemi/src/components/SignInModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="modal-overlay" v-show="showModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Sign In</h5>
+          <button type="button" class="btn-close" @click="$emit('close-modal')" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="remember-me">
+              <label class="form-check-label" for="remember-me">Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign In</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignInModal",
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;  
+  }
+</style>
+
